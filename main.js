3afde76ecdbcf494e9ded144e7756fa5e728657f
Stephen Settle <ssettle3@gmail.com>
2015-01-15 17:46:08 -0500

    Answers 1-3

---
+++ main.js
@@ -0,0 +1,67 @@
+
+// ****************** 1. How to calculate the average price of all items. *************************//
+// ============================================================================================================================================
+
+// I want to pull all the prices in all objects down and list them out.
+// I want to add all the prices together to one big lump sump.
+// Next I want to take that sum and divide it by the total number of items.
+
+
+var priceArray = items.map(function(itemPrice, i, a){
+	return itemPrice.price;
+});
+console.log(priceArray);
+
+var priceSum = priceArray.reduce(function(current, next){
+	return current + next;
+});
+console.log(priceSum);
+
+var priceAvg = (priceSum / items.length);
+console.log(priceAvg);
+
+// This yeilds a value of 23.625999999999998. So.....
+
+var Avg = priceAvg.toFixed(2);
+console.log("The average price is " + Avg);
+
+
+
+//**************** 2. How to get an array of items that cost between $14.00 and $18.00 USD. ******************* //
+//================================================================================================================================
+
+
+var itemArray = items.filter(function(item){
+  return item.price >= 14  && item.price <= 18;
+});
+
+var nameArray = itemArray.map(function(name){
+	return  name.title;
+});
+console.log("Items that cost between $14.00 USD and $18.00 USD: " + nameArray);
+
+
+//*********************** 3. How to find the item with a "GBP" currency code and print its name and price. ***********************//
+//==================================================================================================================================
+// Set up a filter to search for the currency_code GPB
+
+var moneyArray = items.filter(function(money){
+	return money.currency_code === GBP;
+});
+var nameArray = moneyArray.map(function(name){
+	return name.title;
+	return name.price;
+});
+
+
+
+// 4. How to find which items are made of wood.
+
+
+
+// 5. How to find which items are made of eight or more materials.
+
+
+
+
+// 6. How to calculate how many items were made by their sellers
